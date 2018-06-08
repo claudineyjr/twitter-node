456e0a9902d5ad22f13c318055dbe4792d4db7ce
Claudiney Junior <p000claujunior@prservicos.com.br>
2018-06-08 15:45:03 -0300

    Credentials file model and README update

---
+++ app/config/credentials.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  '',
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+}
